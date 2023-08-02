384758a276588432f8584827b9f71a81caaae55f
maritamis <tamismm12@gmail.com>
2023-08-02 15:44:51 -0700

    "updated Author Graphql"

--- bookstore/src/main/java/com/company/bookstore/controllers/GraphController.java
+++ bookstore/src/main/java/com/company/bookstore/controllers/GraphController.java
@@ -1,13 +1,12 @@
 package com.company.bookstore.controllers;
 
 import com.company.bookstore.models.Author;
-import com.company.bookstore.models.Book;
 import com.company.bookstore.models.Publisher;
+import com.company.bookstore.repositories.AuthorRepository;
 import com.company.bookstore.repositories.PublisherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -16,8 +15,20 @@ import java.util.Optional;
 @Controller
 public class GraphController {
     @Autowired
+    AuthorRepository authorRepository;
+    @Autowired
     PublisherRepository publisherRepository;
 
+
+    @QueryMapping
+    public Author findAuthorById(@Argument Integer id){
+        Optional<Author> returnVal = authorRepository.findById(id);
+        if(returnVal.isPresent()){
+            return returnVal.get();
+        } else{
+            return null;
+        }
+    }
     @QueryMapping
     public Publisher findPublisherById(@Argument Integer id) {
         Optional<Publisher> returnVal = publisherRepository.findById(id);
@@ -27,16 +38,4 @@ public class GraphController {
             return null;
         }
     }
-
-    @SchemaMapping
-    public Publisher publisher(Book book){
-        Optional<Publisher>returnVal = publisherRepository.findById(book.getPublisherId());
-        if (returnVal.isPresent()) {
-            return returnVal.get();
-        } else {
-            return null;
-        }
-    }
-
-
 }
